2580fc69b2de52ffb55c4bf42044683007204b49
Nicholas Charette <charettenr@gmail.com>
2020-12-07 12:19:28 -0600

    Day 5

---
+++ 05-1/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+typedef unsigned long ul;
+
+enum State {
+  NONE,
+  LABEL,
+  FIELD,
+};
+
+int main() {
+  ifstream input ("in.txt");
+
+  int max_id = 0;
+
+  while(!input.eof()) {
+    string pass;
+    getline(input, pass);
+
+    if(pass.size() < 6) { // to weed out empty lines
+      continue;
+    }
+
+    int i = 0;
+    int row;
+    int column;
+    {
+      int current_row_lower_bound = 0;
+      int row_lower_bound_increase = 64;
+      for(; i < 7; i++) {
+        if(pass[i] == 'B') {
+          current_row_lower_bound += row_lower_bound_increase;
+        }
+        row_lower_bound_increase /= 2;
+      }
+
+      row = current_row_lower_bound;
+    }
+
+    {
+      int current_col_lower_bound = 0;
+      int col_lower_bound_increase = 4;
+      for(; i < 10; i++) {
+        if(pass[i] == 'R') {
+          current_col_lower_bound += col_lower_bound_increase;
+        }
+        col_lower_bound_increase /= 2;
+      }
+
+      column = current_col_lower_bound;
+    }
+
+    int id = row*8 + column;
+
+    if(max_id < id) {
+      max_id = id;
+    }
+  }
+
+  cout << max_id << endl;
+}
